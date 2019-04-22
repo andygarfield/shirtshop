b903dc49f9f9caaa407806b41a2c64f12d357588
Andy Garfield <andyjgarfield@gmail.com>
2019-04-22 18:14:22 -0400

    Added to build script

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 basedir=$PWD
 
+# clone Turing repo and copy product images
+git clone https://github.com/zandoan/turing-fullstack.git
+cp -r ./turing-fullstack/Images/product_images ./server/public
+
 # build client code and move to server dir
 cd ./client
 npm run build
